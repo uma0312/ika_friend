f1ee5db8b697f53bed0943f9e39e50e834d1c5ad
Yuuma Nagao <yu050312@gmail.com>
2016-02-05 14:44:18 +0900

    Fix Bug

--- content_script.js
+++ content_script.js
@@ -5,27 +5,34 @@ $(function(){
 
 		var player_names = "";
 		var player_names_array = new Array();
-		//console.log($(".friend.status-online.status-playing > .friend-name-wrapper > .friend-text-box > .typography-player-name > .text-link").text());
+
+		// オンラインのプレイヤー名を保存
 		$(".friend.status-online.status-playing > .friend-name-wrapper > .friend-text-box > .typography-player-name > .text-link").each(function(index, element) {
 			var player_name = $(element).text() // プレイヤー名
 			player_names += player_name + "\n";
 			player_names_array.push(player_name);
 		});
-		player_names += "がSplatoonをプレイ中です！"
 
-		// sessionStorageが空ならば現在のプレイヤーを格納
-		if (! sessionStorage.online_friends) {
-			console.log("aaa");
+		if (player_names !== "") {
+			player_names += "がSplatoonをプレイ中です！";
+		}
+
+		if (sessionStorage.length === 0) {
 			sessionStorage.setItem("online_friends", player_names_array);
-			chrome.runtime.sendMessage({msg: player_names}, function(response) {
-				console.log("ccc");
-    			console.log(response);
-			});
+			if (player_names === "") {
+				chrome.runtime.sendMessage({msg: 'Splatoonをプレイ中のフレンドはいません…'}, function(response) {
+    				console.log(response);
+				});
+			} else {
+				chrome.runtime.sendMessage({msg: player_names}, function(response) {
+    				console.log(response);
+				});
+			}
 		}
 		
 		// 前回の確認から変化があれば通知を出す
-		if (player_names_array.toString() != sessionStorage.online_friends.toString()) {
-			console.log("bbb")
+		if (player_names_array.toString() !== sessionStorage.online_friends.toString()) {
+			//console.log("bbb")
 			sessionStorage.setItem("online_friends", player_names_array);
 			chrome.runtime.sendMessage({msg: player_names}, function(response) {
     			console.log(response);
