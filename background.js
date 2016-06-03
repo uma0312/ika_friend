c1305d5365f2bde9e677e0e9b9ed463836c28ebc
Yuuma Nagao <yu050312@gmail.com>
2016-06-03 23:40:24 +0900

    Modify

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/*
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	var response = {data: request.msg};
 	console.log("test")
@@ -7,3 +8,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	sendResponse(response);
 
 })
+*/
+//$(function(){
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		var response = {data: request.msg};
+		console.log("test")
+
+		// 通知を出す
+		console.log(notify.isSupported);
+		//notify.config({autoClose: 0}); // 1000000[ミリ秒後]に通知を閉じる
+		notify.createNotification( 'イカリング', { body: request.msg, icon: './icon.png' } );
+		sendResponse(response);
+	})
+//})
--- content_script.js
+++ content_script.js
@@ -1,49 +1,30 @@
-$(function(){
+var player_names = "";
+var player_names_array = new Array();
 
-	// 3秒後に処理開始
-	setTimeout(function(){
-
-		var player_names = "";
-		var player_names_array = new Array();
-
-		// オンラインのプレイヤー名を保存
-		$(".friend.status-online.status-playing > .friend-name-wrapper > .friend-text-box > .typography-player-name > .text-link").each(function(index, element) {
-			var player_name = $(element).text() // プレイヤー名
-			player_names += player_name + "\n";
-			player_names_array.push(player_name);
-		});
-
-		if (player_names !== "") {
-			player_names += "がSplatoonをプレイ中です！";
-		}
-
-		if (sessionStorage.length === 0) {
-			sessionStorage.setItem("online_friends", player_names_array);
-			if (player_names === "") {
-				chrome.runtime.sendMessage({msg: 'Splatoonをプレイ中のフレンドはいません…'}, function(response) {
-    				console.log(response);
-				});
-			} else {
-				chrome.runtime.sendMessage({msg: player_names}, function(response) {
-    				console.log(response);
-				});
-			}
-		}
-		
-		// 前回の確認から変化があれば通知を出す
-		if (player_names_array.toString() !== sessionStorage.online_friends.toString()) {
-			//console.log("bbb")
-			sessionStorage.setItem("online_friends", player_names_array);
-			chrome.runtime.sendMessage({msg: player_names}, function(response) {
-    			console.log(response);
-			});
-		} else {
-			console.log("No changes");
-		}
-	}, 3000)
-	
-	// 30秒後にリロード
-	setTimeout(function(){
-		location.reload();
-	}, 30000)
+$.getJSON("https://splatoon.nintendo.net/friend_list/index.json", function(json){
+	$.each(json, function(key, value) {
+		console.log(value.mii_name);
+		player_names += value.mii_name + '\n';
+		player_names_array.push(value.mii_name);
+	});
 });
+
+console.log(sessionStorage.online_friends === void 0);
+player_names += 'がSplatoonをプレイ中です！'
+
+if (String(player_names_array) !== String(sessionStorage.online_friends)) {
+	if (player_names !== "") {
+		chrome.runtime.sendMessage({msg: player_names}, function(response) {
+			console.log(response);
+		});
+	} else {
+		chrome.runtime.sendMessage({msg: 'Splatoonをプレイ中のフレンドはいません…'}, function(response) {
+			console.log(response);
+		});
+	}
+}
+	
+// 30秒後にリロード
+setTimeout(function(){
+	location.reload();
+}, 30000)
